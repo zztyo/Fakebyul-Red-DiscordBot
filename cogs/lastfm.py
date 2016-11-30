639c56eafedc8dbde8e05e457f3dde847f8403f3
Sebastian Winkler <sekl@slmn.de>
2016-11-30 16:07:38 +0100

    adds embeds to /lastfm [...]

--- cogs/lastfm.py
+++ cogs/lastfm.py
@@ -1,3 +1,4 @@
+import discord
 import os
 import aiohttp
 import datetime
@@ -60,6 +61,8 @@ Will remember your username after setting one. [p]lastfm last @username will bec
     @_lastfm.command(pass_context=True, no_pm=True, name='info')
     async def _info(self, context, *username: str):
         """Retrieve general information"""
+        embedData = None
+        message = ""
         if self.api_key != '':
             if not username:
                 settings = dataIO.load_json(self.settings_file)
@@ -91,16 +94,31 @@ Will remember your username after setting one. [p]lastfm last @username will bec
                 user = data['user']['name']
                 playcount = data['user']['playcount']
                 registered = data['user']['registered']['unixtime']
-                registered = datetime.datetime.fromtimestamp(int(registered)).strftime('%Y-%m-%d %H:%M:%S')
+                registered = datetime.datetime.fromtimestamp(int(registered))
                 profile = data['user']['url']
-                message = 'Last.fm profile of {}\n\nScrobbles: {}\nRegistered: {}\nProfile: {}'.format(user, playcount, registered, profile)
+
+                days_since = (datetime.datetime.now() - registered).days
+
+                embedData = discord.Embed(
+                    title="{0}'s profile".format(user),
+                    url=profile,
+                    colour=discord.Colour(value=int("B90000", 16)))
+                embedData.add_field(name="Scrobbles", value=str(playcount))
+                embedData.add_field(name="Registered", value=str("{0}\n({1} days ago)".format(registered.strftime('%Y-%m-%d %H:%M:%S'), days_since)))
+                embedData.set_author(name="last.fm", url="https://www.last.fm/")
+                #message = 'Last.fm profile of {}\n\nScrobbles: {}\nRegistered: {}\nProfile: {}'.format(user, playcount, registered, profile)
         else:
             message = 'No API key set for Last.fm. Get one at http://www.last.fm/api'
-        await self.bot.say('```{}```'.format(message))
+        if embedData != None:
+            await self.bot.say(embed=embedData)
+        else:
+            await self.bot.say('```{}```'.format(message))
 
     @_lastfm.command(pass_context=True, no_pm=True, name='last', aliases=['lp'])
     async def _last(self, context, *username: str):
         """Shows the last 10 played songs"""
+        embedData = None
+        message = ""
         if self.api_key != '':
             if not username:
                 settings = dataIO.load_json(self.settings_file)
@@ -130,26 +148,38 @@ Will remember your username after setting one. [p]lastfm last @username will bec
                 message = '{}'.format(data['message'])
             else:
                 user = data['recenttracks']['@attr']['user']
-                message = '```Last 10 songs played by {}\n\n'.format(user)
+
+                embedData = discord.Embed(
+                    title="{0}\'s last 10 songs played".format(user),
+                    url="http://www.last.fm/user/{0}".format(user),
+                    colour=discord.Colour(value=int("B90000", 16)))
+                embedData.set_author(name="last.fm", url="https://www.last.fm/")
+                #message = '```Last 10 songs played by {}\n\n'.format(user)
                 for i, track in enumerate(data['recenttracks']['track'], 1):
                     try:
                         if track['@attr']['nowplaying'] == 'true':
-                            nowplaying = '(Now playing) '
+                            nowplaying = '(Now playing)'
                     except KeyError:
                         nowplaying = ''
                     artist = track['artist']['#text']
                     song = track['name']
-                    message += '{} {}{} - {}\n'.format(str(i).ljust(4), nowplaying, artist, song)
+                    embedData.add_field(name="{0} {1}".format(str(i).ljust(4), nowplaying), value="{0} by {1}".format(song, artist), inline=False)
+                    #message += '{} {}{} - {}\n'.format(str(i).ljust(4), nowplaying, artist, song)
                     if i > 9:
                         break
-                message += '```'
+                #message += '```'
         else:
             message = 'No API key set for Last.fm. Get one at http://www.last.fm/api'
-        await self.bot.say(message)
+        if embedData != None:
+            await self.bot.say(embed=embedData)
+        else:
+            await self.bot.say(message)
 
     @_lastfm.command(pass_context=True, no_pm=True, name='toptracks', aliases=['tracks', 'ttr'])
     async def _toptracks(self, context, *username: str):
         """Top 10 most played songs"""
+        embedData = None
+        message = ""
         if self.api_key != '':
             if not username:
                 settings = dataIO.load_json(self.settings_file)
@@ -179,19 +209,32 @@ Will remember your username after setting one. [p]lastfm last @username will bec
                 message = '{}'.format(data['message'])
             else:
                 user = data['toptracks']['@attr']['user']
-                message = 'Top songs played by {0}\n\n'.format(user)
+                #message = 'Top songs played by {0}\n\n'.format(user)
+
+                embedData = discord.Embed(
+                    title="{0}'s top songs played".format(user),
+                    url="http://www.last.fm/user/{0}".format(user),
+                    colour=discord.Colour(value=int("B90000", 16)))
+                embedData.set_author(name="last.fm", url="https://www.last.fm/")
+
                 for i, track in enumerate(data['toptracks']['track'], 1):
                     artist = track['artist']['name']
                     song = track['name']
-                    message += '{} {} - {}\n'.format(str(i).ljust(4), artist, song)
+                    embedData.add_field(name=str(i).ljust(4), value="{0} by {1}".format(song, artist), inline=False)
+                    #message += '{} {} - {}\n'.format(str(i).ljust(4), artist, song)
 
         else:
             message = 'No API key set for Last.fm. Get one at http://www.last.fm/api'
-        await self.bot.say('```{}```'.format(message))
+        if embedData != None:
+            await self.bot.say(embed=embedData)
+        else:
+            await self.bot.say('```{}```'.format(message))
 
     @_lastfm.command(pass_context=True, no_pm=True, name='topartists', aliases=['artists', 'tar'])
     async def _topartists(self, context, *username: str):
         """Top 10 played artists"""
+        embedData = None
+        message = ""
         if self.api_key != '':
             if not username:
                 settings = dataIO.load_json(self.settings_file)
@@ -222,18 +265,31 @@ Will remember your username after setting one. [p]lastfm last @username will bec
                 message = '{}'.format(data['message'])
             else:
                 user = data['topartists']['@attr']['user']
-                message = 'Top artists played by {}\n\n'.format(user)
+                #message = 'Top artists played by {}\n\n'.format(user)
+
+                embedData = discord.Embed(
+                    title="{0}'s top artists".format(user),
+                    url="http://www.last.fm/user/{0}".format(user),
+                    colour=discord.Colour(value=int("B90000", 16)))
+                embedData.set_author(name="last.fm", url="https://www.last.fm/")
+                
                 for i, artist in enumerate(data['topartists']['artist'], 1):
                     artist_a = artist['name']
-                    message += '{} {}\n'.format(str(i).ljust(4), artist_a)
+                    embedData.add_field(name=str(i).ljust(4), value=str(artist_a), inline=False)
+                    #message += '{} {}\n'.format(str(i).ljust(4), artist_a)
 
         else:
             message = 'No API key set for Last.fm. Get one at http://www.last.fm/api'
-        await self.bot.say('```{}```'.format(message))
+        if embedData != None:
+            await self.bot.say(embed=embedData)
+        else:
+            await self.bot.say('```{}```'.format(message))
 
     @_lastfm.command(pass_context=True, no_pm=True, name='topalbums', aliases=['albums', 'tab'])
     async def _topalbums(self, context, *username: str):
         """Top 10 played albums"""
+        embedData = None
+        message = ""
         if self.api_key != '':
             if not username:
                 settings = dataIO.load_json(self.settings_file)
@@ -264,18 +320,31 @@ Will remember your username after setting one. [p]lastfm last @username will bec
                 message = '{}'.format(data['message'])
             else:
                 user = data['topalbums']['@attr']['user']
-                message = 'Top albums played by {0}\n\n'.format(user)
+                #message = 'Top albums played by {0}\n\n'.format(user)
+
+                embedData = discord.Embed(
+                    title="{0}'s top albums".format(user),
+                    url="http://www.last.fm/user/{0}".format(user),
+                    colour=discord.Colour(value=int("B90000", 16)))
+                embedData.set_author(name="last.fm", url="https://www.last.fm/")
+                
                 for i, album in enumerate(data['topalbums']['album'], 1):
                     albums = album['name']
                     artist = album['artist']['name']
-                    message += '{} {} by {}\n'.format(str(i).ljust(4), albums, artist)
+                    #message += '{} {} by {}\n'.format(str(i).ljust(4), albums, artist)
+                    embedData.add_field(name=str(i).ljust(4), value="{0} by {1}".format(albums, artist), inline=False)
         else:
             message = 'No API key set. Get one at http://www.last.fm/api'
-        await self.bot.say('```{}```'.format(message))
+        if embedData != None:
+            await self.bot.say(embed=embedData)
+        else:
+            await self.bot.say('```{}```'.format(message))
 
     @_lastfm.command(pass_context=True, no_pm=True, name='np')
     async def _np(self, context, *username: str):
         """Shows what you are currently playing"""
+        embedData = None
+        message = ""
         if self.api_key != '':
             if not username:
                 settings = dataIO.load_json(self.settings_file)
@@ -292,6 +361,8 @@ Will remember your username after setting one. [p]lastfm last @username will bec
                 payload = self.payload
                 payload['method'] = 'user.getRecentTracks'
                 payload['username'] = username
+                payload['limit'] = 1
+                payload['extended'] = 1
                 url = 'http://ws.audioscrobbler.com/2.0/?'
                 headers = {'user-agent': 'Red-cog/1.0'}
                 conn = aiohttp.TCPConnector(verify_ssl=False)
@@ -304,25 +375,47 @@ Will remember your username after setting one. [p]lastfm last @username will bec
             if 'error' in data:
                 message = '{}'.format(data['message'])
             else:
+                print(data)
                 user = data['recenttracks']['@attr']['user']
                 track = data['recenttracks']['track']
                 message = ''
                 try:
-                    artist = track[0]['artist']['#text']
+                    #artist = track[0]['artist']['#text']
+                    artist = track[0]['artist']['name']
                     song = track[0]['name']
                     url = track[0]['url']
+                    image = ""
+                    for imageSource in track[0]['image']:
+                        if imageSource['size'] == 'large':
+                            image = imageSource['#text']
+                            break
+
                     try:
                         if track[0]['@attr']['nowplaying'] == 'true':
-                            message = '**{}** is currently listening to **{}** by **{}**'.format(context.message.author.name, song, artist)
+                            #message = '**{}** is currently listening to **{}** by **{}**'.format(context.message.author.name, song, artist)
+                            embedData = discord.Embed(
+                                title="{2} is currently listening to {0} by {1}".format(song, artist, context.message.author.name),
+                                url=url,
+                                colour=discord.Colour(value=int("B90000", 16)))
                     except KeyError:
-                        message = ''
-                    if message == '':
-                        message = '**{}** last listened to **{}** by **{}**'.format(context.message.author.name, song, artist)
+                    #    message = ''
+                        embedData = discord.Embed(
+                            title="{2} last listened to {0} by {1}".format(song, artist, context.message.author.name),
+                            url=url,
+                            colour=discord.Colour(value=int("B90000", 16)))
+                    #if message == '':
+                    #    message = '**{}** last listened to **{}** by **{}**'.format(context.message.author.name, song, artist)
+                    embedData.set_author(name="last.fm", url="https://www.last.fm/")
+                    embedData.set_thumbnail(url=image)
                 except KeyError:
                     message = 'unable to get recent tracks of {}'.format(context.message.author.name)
         else:
             message = 'No API key set for Last.fm. Get one at http://www.last.fm/api'
-        await self.bot.say(message)
+        if embedData != None:
+            await self.bot.say(embed=embedData)
+        else:
+            await self.bot.say(message)
+
 
     @_lastfm.command(pass_context=True, name='apikey')
     @checks.is_owner()
