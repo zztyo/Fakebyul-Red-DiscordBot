01354bada5d7fa15f18c89ad386d626021214935
Sebastian Winkler <sekl@slmn.de>
2016-12-09 13:39:16 +0100

    [prettycards] adds /profilecard-replace command

--- cogs/prettycards.py
+++ cogs/prettycards.py
@@ -69,6 +69,64 @@ class PrettyCards:
             await self.bot.say("I need the `Embed links` permission "
                                "to send this")
 
+    @commands.command(name="profilecard-replace")
+    @checks.mod_or_permissions(administrator=True)
+    async def profilecard_replace(self, editChannel : discord.Channel, editMessageId : str, name : str):
+        """Prints a profile card from the specified profile"""
+
+        try:
+            editMessage = await self.bot.get_message(editChannel, editMessageId)
+        except Exception:
+            await self.bot.say("Unable to get message :warning:")
+            return
+
+        name = name.strip().lower()
+
+        if re.match(r"^([A-Za-z0-9]+)$", name) == None:
+            await self.bot.say("Invalid profile name :warning:")
+            return
+
+        f = "data/prettycards/profile-" + name + ".json"
+        if not dataIO.is_valid_json(f):
+            await self.bot.say("Unable to find profile")
+            return
+
+        profile = dataIO.load_json(f)
+
+        try:
+            colour = int(profile["colour"], 16)
+        except (KeyError, ValueError):
+            colour = ''.join([randchoice('0123456789ABCDEF') for x in range(6)])
+            colour = int(colour, 16)
+
+        data = discord.Embed(
+            description=profile["description"],
+            colour=discord.Colour(value=colour))
+        data.add_field(name="Groups", value=str(profile["groups"]))
+        data.add_field(name="Birthday", value=str(profile["birthday"]))
+
+        data.add_field(name="Position in group", value=str(profile["position_in_group"]))
+        data.add_field(name="Birthplace", value=str(profile["birthplace"]))
+
+        data.add_field(name="Company", value=str(profile["company"]))
+        data.add_field(name="Blood Type", value=str(profile["bloodtype"]))
+
+        data.add_field(name="Languages", value=str(profile["languages"]))
+        data.add_field(name="Weight", value=str(profile["weight"]))
+
+        data.add_field(name="Hobbies", value=str(profile["hobbies"]))
+        data.add_field(name="Height", value=str(profile["height"]))
+
+        data.set_author(name=profile["name"], url=profile["picture"])
+        data.set_thumbnail(url=profile["picture"])
+
+        try:
+            await self.bot.edit_message(editMessage, embed=data)
+            await self.bot.say("Done :ok_hand:")
+        except discord.HTTPException:
+            await self.bot.say("I need the `Embed links` permission "
+                               "to send this")
+
     @commands.command(pass_context=True)
     async def videocard(self, ctx, *, arguments : str):
         """Prints a video card from the arguments <title>;description;thumbnail;hoster=link;..."""
